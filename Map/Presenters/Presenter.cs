43aa0fdf39ee986ada881b32c67a070547aa80d6
hamza <fk_zaidi@esi.dz>
2018-12-12 22:14:24 +0100

    Removed useless code and adde Model View Pesenter Folders

---
+++ Map/Presenters/Presenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Map.Views;
+
+namespace Map.Presenters
+{
+    class Presenter<TView> where TView : class,IView
+    {
+        public TView View { get; private set; }
+
+        public Presenter(TView view)
+        {
+            if (view == null)
+                throw new ArgumentNullException("view");
+
+            View = view;
+            View.Initialize += OnViewInitialize;
+            View.Load += OnViewLoad;
+        }
+
+        protected virtual void OnViewInitialize(object sender, EventArgs e) { }
+
+        protected virtual void OnViewLoad(object sender, EventArgs e) { }
+    }
+}
